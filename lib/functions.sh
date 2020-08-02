b8de7b7c669e1ba916e1a56f2fa0bfc90895b10f
pry0cc <pry0cc@0x00sec.org>
2020-08-02 18:43:38 +0000

    added files

---
+++ lib/functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+log="$DIR/log.txt"
+token=$(cat $DIR/config.json | jq -r .token)
+url="https://ipinfo.io"
+
+echo_n() {
+    echo -e "${White}$1${NC}"
+    echo "NORMAL: $(date): $1" >> $log
+}
+
+echo_error() {
+    echo -e "${Red}$1${NC}"
+    echo "ERROR: $(date): $1" >> $log
+}
+
+log() {
+    echo "LOG: $(date): $1" >> $log
+}
+
+info() {
+    curl -s "$url/$1?token=$token"
+}
+
+asn() {
+    curl -s "$url/$1/json?token=$token"
+}
